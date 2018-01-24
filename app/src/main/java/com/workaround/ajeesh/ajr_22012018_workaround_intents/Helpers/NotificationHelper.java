39bef7de5790b9d8462c9b727f3ba5caf0b2ba47
Ajeesh Kumar Rajan <ajeeshkumar.rajan@gmail.com>
2018-01-24 21:11:55 +0530

    Intent functionalities .
    Creating a intent wch has a category default in it.

---
+++ app/src/main/java/com/workaround/ajeesh/ajr_22012018_workaround_intents/Helpers/NotificationHelper.java
@@ -0,0 +1,45 @@
+package com.workaround.ajeesh.ajr_22012018_workaround_intents.Helpers;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+
+/**
+ * Created by ajesh on 24-01-2018.
+ */
+
+public class NotificationHelper {
+    String logName = "WWI-HLPR-NTFCN";
+    Context mContext;
+
+    public NotificationHelper(Context context) {
+        LogHelper.LogThreadId(logName, "The Notification Helper class is called from Context : " + context.toString());
+        mContext = context;
+    }
+
+    public void createNotification(PendingIntent pendingIntent, int notificationIcon, int notificationId,
+                                   String notificationTitleText, String notificationBodyText) {
+        LogHelper.LogThreadId(logName, "The Notification builder received a pending intent : " + pendingIntent.toString());
+
+        long notificationTimeStamp = System.currentTimeMillis();
+
+        NotificationManager notificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Notification.Builder notificationBuilder = new Notification.Builder(mContext);
+
+        notificationBuilder
+                .setSmallIcon(notificationIcon)
+                .setWhen(notificationTimeStamp)
+                .setContentTitle(notificationTitleText)
+                .setContentText(notificationBodyText)
+                .setContentIntent(pendingIntent);
+
+        Notification notificationForeground = notificationBuilder.getNotification();
+
+        LogHelper.LogThreadId(logName, "The Notification builder: " + notificationBuilder.toString());
+        if (notificationManager != null) {
+            notificationManager.notify(notificationId, notificationForeground);
+        }
+    }
+}
